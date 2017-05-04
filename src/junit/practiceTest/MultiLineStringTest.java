fcd4296c9fd8805e05ab49ac57a5405a33a59523
junjunVC <sechs-053@nifty.com>
2017-05-04 15:18:01 +0900

    joinメソッドのテスト(Matcherあり)

--- src/junit/practiceTest/MultiLineStringTest.java
+++ src/junit/practiceTest/MultiLineStringTest.java
@@ -4,16 +4,22 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import junit.practice.MultiLineString;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MultiLineStringTest {
 
 	@Test
-	public void joinで文字列が連結される_Matcherなし() {
+	public void joinで文字列が連結される_Matcherなし() throws Exception {
 		String lineSeparator = System.getProperty("line.separator");
 		String expected = "Hello" + lineSeparator + "World" + lineSeparator;
 		assertThat(MultiLineString.join("Hello", "World"), is(expected));
+	}
+
+	@Test
+	public void joinで文字列が連結される_Matcherあり() throws Exception {
+		String lineSeparator = System.getProperty("line.separator");
+		String expected = "Hello" + lineSeparator + "World" + lineSeparator;
+		assertThat(MultiLineString.join("Hello", "World"), is(MultiLineStringMatcher.text(expected)));
 	}
 
 }
